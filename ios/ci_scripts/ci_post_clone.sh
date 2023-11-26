753a1456ccd94e8c645eec404a9fc5a6a933d255
Vitaly Sokolov <sokoloff06@gmail.com>
2023-11-26 18:00:57 +0200

    Fix CI

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -24,6 +24,6 @@ brew install cocoapods
 cd ios && pod install # run `pod install` in the `ios` directory.
 
 # Ensure write permissions for flutter build process
-sudo chown -R $(whoami) /Volumes/workspace/DerivedData/Build/Intermediates.noindex/ArchiveIntermediates/Runner/BuildProductsPath/Release-iphoneos/.last_build_id
+chown -R $(whoami) /Volumes/workspace/DerivedData/Build/Intermediates.noindex/ArchiveIntermediates/Runner/BuildProductsPath/Release-iphoneos/.last_build_id
 
 exit 0
